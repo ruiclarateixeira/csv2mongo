c068298bc3bd6eb7c82dffe18ab2de2500208d66
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2016-06-04 15:52:25 +0100

    Gofmt fixed

--- csv2mongo.go
+++ csv2mongo.go
@@ -1,44 +1,44 @@
 package main
 
-import(
-	 "log"
-	 "encoding/json"
-	 "github.com/ruiclarateixeira/csv2json"
-	 "gopkg.in/mgo.v2"
+import (
+	"encoding/json"
+	"github.com/ruiclarateixeira/csv2json"
+	"gopkg.in/mgo.v2"
+	"log"
 )
 
 func main() {
-    fileName := "/Users/ruijorgeclarateixeira/Development/CodingProjects.csv"
-    url := "127.0.0.1"
-    database := "myapps"
-    collection := "apps"
+	fileName := "/Users/ruijorgeclarateixeira/Development/CodingProjects.csv"
+	url := "127.0.0.1"
+	database := "myapps"
+	collection := "apps"
 
-    result, Err := csv2json.ReadFile(fileName)
+	result, Err := csv2json.ReadFile(fileName)
 
-    if Err != nil {
-    	log.Fatal(Err)
-    }
+	if Err != nil {
+		log.Fatal(Err)
+	}
 
-    session, Err := mgo.Dial(url)
+	session, Err := mgo.Dial(url)
 
-    if Err != nil {
-    	log.Fatal(Err)
-    }
+	if Err != nil {
+		log.Fatal(Err)
+	}
 
-    jsonResult := make([]map[string]string, 0)
-    Err = json.Unmarshal(result, &jsonResult) 
+	jsonResult := make([]map[string]string, 0)
+	Err = json.Unmarshal(result, &jsonResult)
 
-    if Err != nil { 
-        log.Fatal(Err)
-    } 
+	if Err != nil {
+		log.Fatal(Err)
+	}
 
-    c := session.DB(database).C(collection)
+	c := session.DB(database).C(collection)
 
-    for _, document := range jsonResult {
-    	Err = c.Insert(document) 
+	for _, document := range jsonResult {
+		Err = c.Insert(document)
 
-	    if Err != nil { 
-	        log.Fatal(Err)
-	    }	
-    }
+		if Err != nil {
+			log.Fatal(Err)
+		}
+	}
 }
