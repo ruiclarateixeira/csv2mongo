189c7c7d22be76dbc676c0d393a293ceea5072b7
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2016-06-04 15:51:36 +0100

    Functionality is working. No tests and need to run through effective go manual

--- csv2mongo.go
+++ csv2mongo.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import(
+	 "log"
+	 "encoding/json"
+	 "github.com/ruiclarateixeira/csv2json"
+	 "gopkg.in/mgo.v2"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fileName := "/Users/ruijorgeclarateixeira/Development/CodingProjects.csv"
+    url := "127.0.0.1"
+    database := "myapps"
+    collection := "apps"
+
+    result, Err := csv2json.ReadFile(fileName)
+
+    if Err != nil {
+    	log.Fatal(Err)
+    }
+
+    session, Err := mgo.Dial(url)
+
+    if Err != nil {
+    	log.Fatal(Err)
+    }
+
+    jsonResult := make([]map[string]string, 0)
+    Err = json.Unmarshal(result, &jsonResult) 
+
+    if Err != nil { 
+        log.Fatal(Err)
+    } 
+
+    c := session.DB(database).C(collection)
+
+    for _, document := range jsonResult {
+    	Err = c.Insert(document) 
+
+	    if Err != nil { 
+	        log.Fatal(Err)
+	    }	
+    }
 }
